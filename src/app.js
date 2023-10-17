f7049d1f11618f15e60ebccb063ea9548f0a4788
sobPare <143205056+sobPare@users.noreply.github.com>
2023-10-16 18:51:00 -0800

    api adj

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
+function displayTemp(response) {
+  let temperatureElement = document.querySelector("#current-temp");
+  let cityElement = document.querySelector("#city");
+  let weahterDescription = document.querySelector("#weather-description");
+  let windSpeed = document.querySelector("#wind-speed");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  weahterDescription.innerHTML = response.data.condition.description;
+  windSpeed.innerHTML = response.data.wind.speed;
+}
 let apiKey = "810af8c18eeb9te5oaa24fe22fec331a";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=paris&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayTemp);

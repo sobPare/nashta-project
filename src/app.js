04e11a86eb1710fdbccb3c27f220cdf57fad5330
sobPare <143205056+sobPare@users.noreply.github.com>
2023-10-16 21:55:04 -0800

    setting date and icon

--- src/app.js
+++ src/app.js
@@ -1,12 +1,43 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 function displayTemp(response) {
   let temperatureElement = document.querySelector("#current-temp");
   let cityElement = document.querySelector("#city");
   let weahterDescription = document.querySelector("#weather-description");
   let windSpeed = document.querySelector("#wind-speed");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   weahterDescription.innerHTML = response.data.condition.description;
   windSpeed.innerHTML = response.data.wind.speed;
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  console.log(response.data.condition.icon_url);
 }
 let apiKey = "810af8c18eeb9te5oaa24fe22fec331a";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=paris&key=${apiKey}`;
